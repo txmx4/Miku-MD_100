c9eeae75fbb253dba03c84a2a1f3a6b70687ce98
txmx4 <123477527+txmx4@users.noreply.github.com>
2023-01-26 17:56:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['4915212908434']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.OwnerNumber = ['4915212908434']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.ownertag = ['4915212908434']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.Owner = ['4917662927731','491777062822']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.OwnerNumber = ['4917662927731','491777062822']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.ownertag = ['4917662927731','491777062822]  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
 global.BotName = "Miku"
-global.packname = "Erstellt Für ✨️MAGIC✨️"
-global.author = "✨️MAGIC✨️"
-global.OwnerName = "✨️MAGIC✨️"
+global.packname = "Erstellt Für User"
+global.author = "M&A Inc."
+global.OwnerName = "M&A Inc."
 global.BotSourceCode = "https://github.com/Fw437f/2084" // -- Please don't change -- //
 global.SupportGroupLink = "https://chat.whatsapp.com/C4UvfSXyYZoDSV3gJDyCm3"  // -- Please don't change -- //
 global.sessionName = "session"  // -- Don't change otherwise bot will not work -- //
 
 
 
-global.prefa = ['-'] // Prefix ---  change it to anythong you want except '@' //
+global.prefa = ['#'] // Prefix ---  change it to anythong you want except '@' //
 
 
 global.location = "West Berlin"
@@ -28,7 +28,7 @@ global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
 global.websitex = "https://github.com/Fw437f/2084"
-global.lolhuman = "✨️MAGIC✨️"
+global.lolhuman = "M&A Inc."
 
 
 global.BotLogo = fs.readFileSync("./Assets/pic1.jpg")

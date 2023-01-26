b82c944207379f9fd9e7e35977c00d8d987629eb
txmx4 <123477527+txmx4@users.noreply.github.com>
2023-01-26 22:22:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['4917662927731','491777062822']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.OwnerNumber = ['4917662927731','491777062822']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.ownertag = ['4917662927731','491777062822']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.Owner = ['4917662927731']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.OwnerNumber = ['4917662927731']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.ownertag = ['4917662927731']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
 global.BotName = "Miku"
 global.packname = "Erstellt Für User"
 global.author = "M&A Inc."
